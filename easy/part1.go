af553444ec443b4feb2c52721969f3f3547ecf6d
Kevin Vargas <vargaskevv@gmail.com>
2024-07-19 03:27:24 -0400

    merge two sorted lists

--- easy/part1.go
+++ easy/part1.go
@@ -61,3 +61,27 @@ func IsValidParentheses(s string) bool {
 	}
 	return len(stack) == 0
 }
+
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	var head *ListNode
+	if list1.Val < list2.Val {
+		head = list1
+		head.Next = MergeTwoLists(list1.Next, list2)
+	} else {
+		head = list2
+		head.Next = MergeTwoLists(list1, list2.Next)
+	}
+	return head
+}
